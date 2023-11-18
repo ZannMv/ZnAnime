3022ee5f98ce18b8ec557ef86794f1d7a1f5dbb1
ZannMv <116694002+ZannMv@users.noreply.github.com>
2023-11-18 14:22:15 +0700

    Update index.js

--- src/layout/index.js
+++ src/layout/index.js
@@ -5,7 +5,7 @@ import { Navbar, Footer } from "@/components";
 const Layout = ({ children }) => (
   <>
     <Head>
-      <title>ROnime</title>
+      <title>ZnAnime</title>
       <meta
         name="viewport"
         content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
